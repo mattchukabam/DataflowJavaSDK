84c95e11ffff9b27a6cabdfa39fd9e1086066b15
Matt Chu <matt.chu@gmail.com>
2015-04-27 12:04:43 -0700

    wip

--- wc-local.sh
+++ wc-local.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 infile=checkstyle.xml
-outfile=/tmp/a
+#outfile=local-out
+outfile=gs://mchu/wc-from-local
+
+#rm -vf ${outfile}*
+
+mvn bundle:bundle -pl examples
 
 mvn exec:java -pl examples \
   -Dexec.mainClass=com.google.cloud.dataflow.examples.WordCount \
   -Dexec.args="--input=${infile} --output=${outfile}"
+
+#cat ${outfile}* | head -n 10
--- wc-remote.sh
+++ wc-remote.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+infile=checkstyle.xml
+#infile=gs://dataflow-samples/shakespeare/kinglear.txt
+outfile="gs://mchu/outwhere/file1"
 project=kabam-bi-adhoc
 staging=gs://mchu/somewhere
-output="--output=gs://mchu/outwhere/file1"
-opts="--filesToStage=checkstyle.xml ${output}"
+#opts="--filesToStage=checkstyle.xml ${outfile}"
 
 mvn bundle:bundle -pl examples
 
-java -cp examples/target/google-cloud-dataflow-java-examples-all-bundled-manual_build.jar \
-  com.google.cloud.dataflow.examples.WordCount \
-  --project=${project} \
-  --stagingLocation=${staging} \
-  --runner=BlockingDataflowPipelineRunner \
-  ${opts}
+#java -cp examples/target/google-cloud-dataflow-java-examples-all-bundled-manual_build.jar \
+mvn exec:java -pl examples \
+  -Dexec.mainClass=com.google.cloud.dataflow.examples.WordCount \
+  -Dexec.args="--output=${outfile} --project=${project} --stagingLocation=${staging} --runner=BlockingDataflowPipelineRunner"
+  #-Dexec.args="--filesToStage=${infile} --output=${outfile} --project=${project} --stagingLocation=${staging} --runner=BlockingDataflowPipelineRunner"
+  #-Dexec.args="--input=${infile} --filesToStage=${infile} --output=${outfile} --project=${project} --stagingLocation=${staging} --runner=BlockingDataflowPipelineRunner"
